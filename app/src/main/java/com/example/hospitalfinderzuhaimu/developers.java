dbfae504c92adbf587b1a5a23606398de88f14a4
2021113433 <106805163+2021113433@users.noreply.github.com>
2022-07-22 01:27:46 +0800

    Saya suka nasi ayam

---
+++ app/src/main/java/com/example/hospitalfinderzuhaimu/developers.java
@@ -0,0 +1,29 @@
+package com.example.hospitalfinderzuhaimu;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class developers extends AppCompatActivity {
+    Button button;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_developers);
+
+        button = (Button) findViewById(R.id.btnhome);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                userdashboard();
+            }
+        });
+    }
+    public void userdashboard(){
+        Intent intent = new Intent(this, UserDashboard.class);
+        startActivity(intent);
+    }
+}
